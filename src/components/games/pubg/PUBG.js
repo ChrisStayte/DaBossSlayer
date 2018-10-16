0eb7cdd61dcf32003fb3a6569ba1d538e9332b4f
Chris Stayte <ctstayte@gmail.com>
2018-10-16 11:48:50 -0400

    testing

---
+++ src/components/games/pubg/PUBG.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class PUBG extends Component {
+  render() {
+    return <div>PUBG</div>;
+  }
+}
---
+++ src/components/layout/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a href="#" className="navbar-brand">
+          DaBossSlayer
+        </a>
+      </nav>
+    );
+  }
+}
