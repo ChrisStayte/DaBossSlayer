e9edc2b8c530a2284c9081224971ce2665dad5fd
Chris Stayte <ctstayte@gmail.com>
2018-10-16 14:15:43 -0400

    Updates

---
+++ precache-manifest.30b6e336d009cbfd55cc2b424e7f8cef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DaBossSlayer/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "62f8e29099b850ded40c",
+    "url": "/DaBossSlayer/static/js/runtime~main.62f8e290.js"
+  },
+  {
+    "revision": "1fbfa1593a4a30bdd657",
+    "url": "/DaBossSlayer/static/js/main.1fbfa159.chunk.js"
+  },
+  {
+    "revision": "4f2c36e391b74fb820b4",
+    "url": "/DaBossSlayer/static/js/1.4f2c36e3.chunk.js"
+  },
+  {
+    "revision": "1fbfa1593a4a30bdd657",
+    "url": "/DaBossSlayer/static/css/main.0245650a.chunk.css"
+  },
+  {
+    "revision": "4f2c36e391b74fb820b4",
+    "url": "/DaBossSlayer/static/css/1.72552199.chunk.css"
+  },
+  {
+    "revision": "69854622cf9bab1e9a9571b58881d097",
+    "url": "/DaBossSlayer/index.html"
+  }
+];
